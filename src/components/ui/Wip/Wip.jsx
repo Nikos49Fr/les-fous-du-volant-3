304d86359e245387f1a7a061fd3934f0abbcaa33
Nicolas Grignard <nicolas4980@gmail.com>
2026-03-06 14:52:13 +0100

    Mise à jour du composant WIP avec nouvelle image et texte arrangé.

--- src/components/ui/Wip/Wip.jsx
+++ src/components/ui/Wip/Wip.jsx
@@ -1,28 +1,19 @@
 import './Wip.scss';
-import wipRandyAtWork from '../../../assets/images/wip/wip_randy_at_work.webp';
-import wipRandyVariant from '../../../assets/images/wip/wip_randy_variant.webp';
-import wipRandyEndWork from '../../../assets/images/wip/wip_randy_end_work.webp';
+import wipScene from '../../../assets/images/wip/wip_scene.webp';
 
 export default function Wip() {
     return (
         <div className="app-wip">
-            <p className="app-wip__text">Page en cours de construction</p>
-            {/* <img
-                className="app-wip__image"
-                src={wipRandyAtWork}
-                alt="Personnage en train de travailler"
-            /> */}
-            <img
-                className="app-wip__image"
-                src={wipRandyVariant}
-                alt="Personnage en train de travailler, variante"
-            />
-            {/* <img
-                className="app-wip__image"
-                src={wipRandyEndWork}
-                alt="Personnage en train de travailler"
-            /> */}
-            <p className="app-wip__text">Randy variant "at work"</p>
+            <p className="app-wip__text">🚧 Page en cours de construction 🚧 <br />
+            Nous travaillons dur pour que cette section soit prête rapidement afin de vous offir la meilleure expérience.</p>
+            <div className="app-wip__media-content">
+                <img
+                    className="app-wip__image"
+                    src={wipScene}
+                    alt="Personnage en train de travailler"
+                />
+            </div>
+            <p className="app-wip__text"><em>Hommage à nos 2 organisateurs <strong>RandyComicsFr</strong> et <strong>Lord_Viserion</strong></em></p>
         </div>
     );
 }
